2d5841a7be5ab07795eaf9bed78082f69a4cd618
Marcos Vazquez <marcosvazquezfer@gmail.com>
2019-05-20 23:50:16 +0200

    Añadida instalación offline

--- offline_installation.py
+++ offline_installation.py
@@ -2,162 +2,163 @@ import os
 import sys
 import subprocess as s
 
-from lib.helpers.colors import *
 
+def check_permissions_error(check_errors):
+    """ 
+    This function checks if an error is caused by user permissions
 
-def permissions_error(err):
-    """ Checks if an error is caused by lack of user permissions.
-
-        :param err: Error to check
-        :type err: tuple
-        :return: True if the error is about permissions.
-        :return: False if the error is not about permissions
+        :param check_errors: Error to check
+        :type check_errors: tuple
+        :return: If the error is about permissions return True.
+        :return: If the error is not caused about permissions return False
         :rtype: bool
     """
 
-    if ('permitted' in err or 'permissions' in err) and 'DEPRECATION' not in err:
+    if ('permitted' in check_errors or 'permissions' in check_errors) and 'DEPRECATION' not in check_errors:
         return True
+
     return False
 
 
-def install_nmap():
-    """ Linux system based nmap installation, for Red Hat and Debian distributions.
+def nmap_installation():
+    """ 
+    Nmap offline installation for distributions based on Debian and Red Hat.
 
-    As the nmap package is a .rpm file, Debian distributions need alien to install it.
-    Suppose that the system is a Red Hat system at first, if not, install
-    alien and then the .rpm file with it.
+    If the nmap package is a .rpm file, Debian distributions need alien to install it.
+    Suppose that the system is a Red Hat system at first, if not, install alien and then the .rpm file with it.
     """
 
-    pkt_dir = './packages/os'
+    directory_os_packages = './packages/os'
     alien_file = None
     nmap_file = None
 
-    # Check for nmap file
+    # Search nmap file
     try:
-        nmap_file = [f for f in os.listdir(pkt_dir) if 'nmap' in f][0]
+        nmap_file = [f for f in os.listdir(directory_os_packages) if 'nmap' in f][0]
     except IndexError:
-        halt_fail('Nmap(.rpm) package not found under ./packages/os')
+        print('ATENTION: nmap package is not in the /package/os folder')
 
-    # Execute RPM file installation with yum, Red Hat distribution
-    nmap_install = s.Popen(['yum', 'install', pkt_dir + '/' + nmap_file], stderr=s.PIPE)
+    # For distributions based on Red Hat, execute installation with yum
+    nmap_install = s.Popen(['yum', 'install', directory_os_packages + '/' + nmap_file], stderr=s.PIPE)
     errors = nmap_install.communicate()
-    # If errors, it probably means it is a Debian based SO
-    if len(errors):
-        if permissions_error(errors):
-            halt_fail('Popped error about permissions, try executing "sudo python offline_install.py"')
-        print_fail('Could not install nmap with yum:\n{}'.format(errors))
 
-        # Ask user if he wants to try installation with alien. Keep asking until a valid answer
+    # If exists any errors, it probably means that it is a distribution based on Debian
+    if len(errors) > 0:
+        if check_permissions_error(errors) == True:
+            print('ATENTION: Found an error of permissions. Execute "sudo python offline_install.py"')
+        print('ATENTION: It is not possible to use yum to install nmap:\n{}'.format(errors))
+
+        # Ask user if he wants to try installation with alien
         option = 'k'
         while option.lower() not in ['y', 'n']:
-            option = raw_input('Do you want to attempt nmap installation with alien? If your system is Red Hat based,'
-                               ' please re-read the STDERR from above before answering [Y/n]: ')
+            option = raw_input('Do you want to try nmap installation with alien? If you have an O.S. based on Red Hat, read the previous error (Y/n): ')
 
-        # If user says yes
         if 'y' in option.lower():
-            print_warning('Attempting alien installation.')
+            print('Trying alien installation...')
 
-            # Check for alien file
+            # Search alien file
             try:
-                alien_file = [f for f in os.listdir(pkt_dir) if 'alien' in f][0]
+                alien_file = [f for f in os.listdir(directory_os_packages) if 'alien' in f][0]
             except IndexError:
-                halt_fail('Alien(.deb) package not found under ./packages/os')
+                print('ATENTION: alien package was not found in the /packages/os folder')
 
             # Install alien
-            alien_install = s.Popen(['dpkg', '-i', pkt_dir + '/' + alien_file], stderr=s.PIPE)
+            alien_install = s.Popen(['dpkg', '-i', directory_os_packages + '/' + alien_file], stderr=s.PIPE)
             errors = alien_install.communicate()
-            # If errors installing alien, halt execution
-            if len(errors):
-                if permissions_error(errors):
-                    halt_fail('Popped error about permissions, try executing "sudo python offline_install.py"')
-                halt_fail('Could not install alien, and therefore nmap. STDERR says:\n{}'.format(errors))
-            print_success('Successfully installed alien')
+
+            # Stops the execution if exists errors installing alien
+            if len(errors) > 0:
+                if check_permissions_error(errors) == True:
+                    print('ATENTION: Found an error of permissions. Execute "sudo python offline_install.py"')
+                print('ATENTION: It is not possible to install alien and then nmap either:\n{}'.format(errors))
+            print_success('Alien was succesfully installed')
 
             # Install nmap with alien
-            nmap_install = s.Popen(['alien', '-i', pkt_dir + '/' + nmap_file], stderr=s.PIPE)
+            nmap_install = s.Popen(['alien', '-i', directory_os_packages + '/' + nmap_file], stderr=s.PIPE)
             errors = nmap_install.communicate()
-            # If errors intalling nmap with alien, halt execution
-            if len(errors):
-                if permissions_error(errors):
-                    halt_fail('Popped error about permissions, try executing "sudo python offline_install.py"')
-                halt_fail('Could not install nmap with alien. STDERR says:\n{}'.format(errors))
 
-        # If user answers no
+            # Stops the exectuion if exists errors installing nmap with alien
+            if len(errors) > 0:
+                if check_permissions_error(errors) == True:
+                    print('ATENTION: Found an error of permissions. Execute "sudo python offline_install.py"')
+                print('ATENTION: It is not possible to use alien to install nmap:\n{}'.format(errors))
+
         else:
-            halt_fail('PSCAD cannot be used without nmap. Halting installation...')
+            print('ATENTION: NetwInv could not use without nmap')
 
-    print_success('Successfully installed nmap')
+    print('Nmap installation was succesfully')
 
 
-def install_module(pkg):
-    """ Installs a given package using the pip command.
+def install_module(package):
+    """ 
+    This function is used to install a package using pip command.
 
-        :param pkg: Package to install
-        :type pkg: str
+        :param package: The package to install
+        :type package: str
     """
 
     with open('/dev/null') as null_file:
-        mod_install = s.Popen(['pip', 'install', 'packages/' + pkg], stderr=s.PIPE, stdout=null_file)
+        mod_install = s.Popen(['pip', 'install', 'packages/' + package], stderr=s.PIPE, stdout=null_file)
     _, errors = mod_install.communicate()
-    # If errors when installing module
-    if len(errors):
-        if permissions_error(errors):
-            halt_fail('Popped error about permissions, try executing "sudo python offline_install.py"')
-        if 'DEPRECATION' not in errors:
-            halt_fail('Could not install module from {}. STDERR says:\n{}'.format(pkg, errors))
 
-    print_success('Successfully installed module from {}'.format(pkg))
+    # If exists any errors installing package
+    if len(errors) > 0:
+        if check_permissions_error(errors) == True:
+            print('ATENTION: Found an error of permissions. Execute "sudo python offline_install.py"')
+
+        if 'DEPRECATION' not in errors:
+            print('ATENTION: It is not possible to install module from {}. STDERR says:\n{}'.format(package, errors))
+
+    print('Module was succesfully installed')
 
 
 def main():
-    """ Checks if pip is on the system, if so, install the dependencies, if not, execute the get-pip.py script
-    to install it
+    """ 
+    Search pip on the system, if exists install dependencies. 
+    If not exists execute get-pip.py script to install it.
     """
 
-    has_pip = False
-    pkg_dir = './packages'
+    pip_exist = False
+    package_folder = './packages'
 
     try:
         with open('/dev/null', 'w') as null_file:
             s.check_call(['nmap',  '-h'], stdout=null_file)
-        print_success('Nmap found on the system.')
+        print('Nmap was found on the system.')
     except s.CalledProcessError:
-        print_fail('Nmap not found on the system, attempting installation...')
-        install_nmap()
+        print('ATENTION: Nmap was not found on the system, trying to install.')
+        nmap_installation()
 
-    # Check if pip is installed
+    # Search pip
     try:
         with open('/dev/null', 'w') as null_file:
             s.check_call(['pip', '-h'], stdout=null_file)
-        has_pip = True
-        print_success('pip found on the system.')
+        pip_exist = True
+        print('pip was found on the system.')
     except s.CalledProcessError:
-        print_fail('pip is not installed in the system. Executing get-pip.py...')
+        print('ATENTION: pip was not found on the system. Executing get-pip.py.')
 
-    # If pip needs to be installed
-    if not has_pip:
+    # It is neccesary to install pip
+    if not pip_exist:
         # Execute the get-pip.py script
         pip_installation = s.Popen(['python', 'packages/get-pip.py'], stderr=s.PIPE)
-        # Store errors
         errors = pip_installation.communicate()
-        # If errors, show them and halt execution
-        if len(errors):
-            print_fail('FATAL: Could not install pip. STDERR says:\n{}'.format(errors))
+
+        # If exists any errors, show them and stops execution
+        if len(errors) > 0:
+            print('ATENTION: Could not install pip. STDERR says:\n{}'.format(errors))
             sys.exit(-1)
 
-        print_success('pip successfully installed.')
+        print('pip successfully installed.')
 
-    # Get all packages under the ./packages directory. All files under './packages' that are not python executable
-    # Do not search recursively
-    packages = [f for f in os.listdir(pkg_dir) if os.path.isfile(os.path.join(pkg_dir, f)) and
+    packages = [f for f in os.listdir(package_folder) if os.path.isfile(os.path.join(package_folder, f)) and
                 '.py' not in f]
 
-    # Install all packages
-    for pkg in packages:
-        install_module(pkg)
+    # Install packages
+    for package in packages:
+        install_module(package)
 
-    print_success('All packages have been successfully installed.')
-
+    print('All the neccesary packages were installed correctly')
 
 if __name__ == '__main__':
     main()
